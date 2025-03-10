2bad47cb46dc9e4ce1c06356e389634a8d0fb212
Cart-Shop <54469728+SIMRAT007@users.noreply.github.com>
2025-03-10 22:35:44 +0530

    contact button href

--- src/components/Banner/Banner2.jsx
+++ src/components/Banner/Banner2.jsx
@@ -34,7 +34,8 @@ const Banner2 = () => {
             viewport={{ once: true }}
             className="flex justify-center gap-6"
           >
-            <button className="primary-btn bg-black text-white">Contact Us For More Info</button>
+            <a href="/contact"> <button className="primary-btn bg-black text-white">Contact Us For More Info</button></a>
+           
             {/* <button className="border-2 border-primary text-primary font-bold px-5 rounded-lg">
               Stay In Touch
             </button> */}
